90759d179e051a0af879c9562ee1a898d9d89852
Arkadiusz Fijalkowski <fijalkowski.arek@gmail.com>
2018-05-12 21:22:11 +0200

    Basic starter Counter-App

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+    };
+    this.doIncrement = this.doIncrement.bind(this);
+    this.doDecrement = this.doDecrement.bind(this);
+  }
+  doIncrement() {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1,
+    }));
+  }
+  doDecrement() {
+    this.setState(prevState => ({
+      counter: prevState.counter - 1,
+    }));
+  }
+  render() {
+    const { counter } = this.state;
+    return (
+      <React.Fragment>
+        <Counter counter={counter} />
+        <button type="button" onClick={this.doIncrement}>
+          +
+        </button>
+        <button type="button" onClick={this.doDecrement}>
+          -
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
+const Counter = ({ counter }) => <div>{counter}</div>;
+
+Counter.defaultProps = {
+  counter: 0,
+};
+
+Counter.propTypes = {
+  counter: PropTypes.number,
+};
+
+export default App;
